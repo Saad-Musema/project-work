604a092142c8799812888be10ddd31cbe6017bb4
samrawitgebremaryam <samrawitgebremaryam121@gmail.com>
2023-04-09 20:18:17 +0900

    choice for the user and decryption operation added

--- Assignment Qn. 2.cpp
+++ Assignment Qn. 2.cpp
@@ -2,12 +2,19 @@
 
 using namespace std;
 
-
 int main(){
- cout<<"This is an encrypter, Please input a 4 digit number: ";
- int data, last_d;
- cin>>data;
- int nums[4];//
+
+ int data,choice;// data is the number the user wants to change
+
+    cout << "please enter 1 for encryption and 2 for decryption";
+    cin >> choice;//choice is what the user wants to be done:encryption or decription
+   switch(choice){
+    case 1:
+{
+     cout<<"This is an Encrypter, Please input a 4 digit number: ";
+    int last_d;
+     cin>>data;
+     int nums[4];
 
  for( int i=3; i>=0; i--){
     last_d = data%10;
@@ -38,7 +45,21 @@ cout<<"The encrypted data is ";
  for( int i=0; i<4; i++){
     cout<<nums[i];
  }
+ }
+    case 2:
+        {
 
+    cout << "This is a decrypter please enter a four-digit integer : ";
+    cin >> data;
+int first,second,third,fourth;
+        first = ((data / 1000) + 3) % 10;
+       second = ((data / 100) % 10 + 3) % 10;
+       third = ((data / 10) % 10 + 3) % 10;
+       fourth = (data % 10 + 3) % 10;
+        cout << "Decrypted number: " << third * 1000 + fourth * 100 + first * 10 + second << endl;
+         break;
+    } default:
+        cout<<" Invalid choice"; }
 
 
 return 0;
