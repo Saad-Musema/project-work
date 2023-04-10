27d5f6876bc4337e417018bb3b96070f95b9cad1
SamiyaYusuf <sammmysamuti@gmail.com>
2023-04-10 15:34:41 +0300

    Question I. drawing the number pattern edited

--- Assignment 1.cpp
+++ Assignment 1.cpp
@@ -8,44 +8,50 @@ int main(){
     int n, i,j,k, x, m;
 
 
-
 cout<<"Please input number of your liking ";
 cin>>m;
 
-for( i = 0; i<=m; i++){
-    for(x = 0; x<=i; x++){
-        if(x==m){
-                break;
-            }
-        cout<<x;
-    }
-    for(j=i+1; j<m; j++){
-        cout<<" ";
-    }
-    for(k =m-i; k>0; k--){
-        cout<<" ";
-    }
-    for( n = i; n>=0; n--){
 
-        cout<<n;
-    }
-    cout<<endl;
-}
-for( i = m-1; i>=0; i--){
-    for(x = 0; x<=i; x++){
-        cout<<x;
-    }
-    for(j=m-i; j>0; j--){
-        cout<<" ";
-    }
-    for(k=m-i; k>1; k--){
-        cout<<" ";
-    }
-    for( n = i; n>=0; n--){
-        cout<<n;
-    }
-    cout<<endl;
-}
+if(m>=1){
 
+    for( i = 0; i<=m; i++){
+        for(x = 0; x<=i; x++){
+            /*if(x==m){
+                    break;
+                }*/
+            cout<<x;
+        }
+        for(j=i+1; j<m; j++){
+            cout<<" ";
+        }
+        for(k =m-i; k>0; k--){
+            cout<<" ";
+        }
+        for( n = i; n>=0; n--){
+
+            cout<<n;
+        }
+        cout<<endl;
+    }
+
+    for( i = m-1; i>=0; i--){
+        for(x = 0; x<=i; x++){
+            cout<<x;
+        }
+        for(j=m-i; j>0; j--){
+            cout<<" ";
+        }
+        for(k=m-i; k>1; k--){
+            cout<<" ";
+        }
+        for( n = i; n>=0; n--){
+            cout<<n;
+        }
+        cout<<endl;
+    }
+}
+else{
+    cout<<"Invalid input";
+}
 return 0;
 }
